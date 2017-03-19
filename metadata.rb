6c1a8ed48996689adc86d7dd18ee5aeebc1b8c52
krowpu <krowpu@tightmail.com>
2017-03-19 14:21:40 +0500

    Add metadata

--- metadata.rb
+++ metadata.rb
@@ -2,4 +2,10 @@ name 'deploy'
 description 'Deployment user'
 license 'MIT'
 
+maintainer 'krowpu'
+maintainer_email 'krowpu@tightmail.com'
+
+source_url 'https://github.com/krowpu/chef-deploy'
+issues_url 'https://github.com/krowpu/chef-deploy/issues'
+
 platform 'ubuntu', '= 16.04'
